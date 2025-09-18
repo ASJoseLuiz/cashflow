c8348a874a903a39f1174167b53411b3b69fe026
José Luiz <joseluix4824@gmail.com>
2025-09-18 20:54:20 -0300

    feat: add email config

--- src/mongo/schemas/user.schema.ts
+++ src/mongo/schemas/user.schema.ts
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    verified: { type: Boolean, required: true}
   },
 });
 
